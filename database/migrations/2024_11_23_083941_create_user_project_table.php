84ce91986965107235c9bf5e9888d6332d91f44a
John Andrei Tacujan <89928907+DreiDev04@users.noreply.github.com>
2024-12-02 07:10:59 +0000

    fix with task.create

--- database/migrations/2024_11_23_083941_create_user_project_table.php
+++ database/migrations/2024_11_23_083941_create_user_project_table.php
@@ -3,7 +3,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProjectUserTable extends Migration
+class CreateUserProjectTable extends Migration
+
 {
     public function up()
     {
--- routes/web.php
+++ routes/web.php
@@ -11,27 +11,22 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome'); // Ensure you have a Welcome component in your resources/js/Pages directory
+    return Inertia::render('Welcome');
 })->name('welcome');
 
-// Auth::routes(['verify' => true]);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get("/dashboard", [DashboardController::class, "index"])->name("dashboard");
     Route::resource("project", ProjectController::class);
     Route::get("/task/my-tasks", [TaskController::class, "myTasks"])->name("task.myTasks");
-    Route::resource("task", TaskController::class);
+    Route::resource("task", TaskController::class); // Resource route already includes the 'create' route
 
-    Route::get('task/create/{project}', [TaskController::class, 'create'])->name('task.create');
-    Route::get('project/{project}/task/{task}', [TaskController::class, 'show'])->name('task.show');
-    Route::get('project/{project}/task/{task}/edit', [TaskController::class, 'edit'])->name('task.edit');
-
+    // Route::get('project/{project}/task/{task}', [TaskController::class, 'show'])->name('task.show');
+    // Route::get('project/{project}/task/{task}/edit', [TaskController::class, 'edit'])->name('task.edit'`);
 
     Route::get('project/{project}/join', [ProjectController::class, 'join'])->name('project.join');
     Route::post('project/{project}/leave', [ProjectController::class, 'leave'])->name('project.leave');
     Route::resource("user", UserController::class);
-
-
 });
 
 Route::middleware('auth')->group(function () {
